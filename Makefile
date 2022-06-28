f4d2fcd714e5a0e6d135aefcd5a5c4191cd5128a
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-06-28 21:05:10 +0800

    Succeeded in spliting PSmodelName in product_key.csv

---
+++ Makefile
@@ -0,0 +1,13 @@
+# Implicit variables
+PY = python
+
+# Source codes
+SRCS = 
+
+# Targets
+MAIN = main.py
+
+all:
+	@$(PY) $(MAIN)
+
+.PHONY:
---
+++ main.py
@@ -0,0 +1,38 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from multiprocessing.sharedctypes import Value
+from unicodedata import category
+import pandas as pd
+import string
+
+# target = "samsung galaxy a03"
+# testcases = ["samsung galaxy a3", "samsunggalaxy a3", "samsung galaxy a 3", "samsung galaxy",
+# 			"galaxy a3 samsung", "galaxy a03 samsung"]
+
+# for i in testcases:
+# 	print(fuzz.ratio(target, i))
+
+# df = pd.read_csv(r"./data/link_name.csv")
+
+# data = df[r"productName from URL"]
+
+# for i in data:
+# 	print(i.lower())
+
+# Split product key into tokens
+pk = pd.read_csv(r"./data/product_key.csv")
+pk = pk[pk['category'] == "Mobile"]
+pk_data = pk[(r"PSmodelName")]
+
+for data in pk_data:
+	space = data.index(' ')
+	try:
+		bracket = data.rfind(r'(')
+		print(data[0:space], end='')
+		print(data[space:bracket], end='')
+		print(data[bracket:])
+	except ValueError as ve:
+		print("", end='')
+		print(data[0:space], end='')
+		print(data[space+1:])
+
