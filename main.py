00d928d2ac5f95d25a2383ee9e13e9327e006660
Han qin Loke <hloke@u82z05s06.42kl.edu.my>
2022-06-30 12:57:53 +0800

    Brand and model matching didn't meet expectation. Recoding...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from curses.ascii import isdigit
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from multiprocessing.sharedctypes import Value
@@ -5,44 +6,78 @@ from unicodedata import category
 import pandas as pd
 import string
 
-# target = "samsung galaxy a03"
-# testcases = ["samsung galaxy a3", "samsunggalaxy a3", "samsung galaxy a 3", "samsung galaxy",
-# 			"galaxy a3 samsung", "galaxy a03 samsung"]
-
-# for i in testcases:
-# 	print(fuzz.ratio(target, i))
-
-# df = pd.read_csv(r"./data/link_name.csv")
-
-# data = df[r"productName from URL"]
-
-# for i in data:
-# 	print(i.lower())
-
-# Split product key into tokens
+# Split product keys into a list of usable info
+# Brand, Model, Model Year, Size
 pk = pd.read_csv(r"./data/product_key.csv")
-pk = pk[pk['category'] == "Mobile"]
+pk = pk[pk['category'] != "Laptop"]
 pk_data = pk[(r"PSmodelName")]
 
+product_key = []
 for data in pk_data:
+	list = []
+	data = data.lower()
 	space = data.index(' ')
-	try:
-		bracket = data.rfind(r'(')
-		print(data[0:space], end='')
-		print(data[space:bracket], end='')
-		print(data[bracket:])
-	except ValueError as ve:
-		print("", end='')
-		print(data[0:space], end='')
-		print(data[space+1:])
+	size_bracket = data.rfind(r'(')
+	if (size_bracket != -1):
+		year_bracket = data.index(r'(')
+		list.append(data[0:space])
+		if (year_bracket != size_bracket):
+			list.append(data[space+1:year_bracket-1])
+			list.append(data[year_bracket:size_bracket-1])
+		else:
+			list.append(data[space+1:size_bracket-1])
+			list.append('NULL')
+		list.append(data[size_bracket:].replace(' ', ''))
+	else:
+		list.append(data[0:space])
+		list.append(data[space+1:])
+	product_key.append(list)
 
+# Simplified scraped data
+db = pd.read_csv(r"./data/complete.csv")
+database = db["productName from URL"]
+
+for i in range(len(database)):
+	l = 0
+	if (database[i][0].isalnum() == False):
+		while (database[i][l].isalpha() == False):
+			l += 1
+	delim = l
+	while delim < len(database[i]):
+		if (database[i][delim] == r'(' or database[i][delim] == r'[' or database[i][delim] == r'*'):
+			break
+		delim += 1
+	database[i] = database[i][l:delim].lower()
+
+# Remove the brand name from each database entry
+def cleanse(database, product_key):
+	for i in range(len(database)):
+		for key in product_key:
+			if (database[i].find(key[0]) != -1):
+				database[i] = database[i].replace(r"\"", '').strip()
+				break
+
+cleanse(database, product_key)
+
+# To see if a product key matches with a database entry
+def matching(key, data):
+	if (data.find(key[0]) != -1 and (data.find(key[1] + ' ') != -1 or data.find(key[1] + r'/') != -1)):
+		return True
+	return False
+
+count = 0
 # # Reading link names
-# ln = pd.read_csv(r"./data/link_name.csv")
-# linkname = ln["productName"]
+for data in database:
+	data = data.lower()
+	for key in product_key:
+		if (matching(key, data) == True):
+			print("TRUE")
+			print("KEY  = ", key)
+			print("DATA = ", data)
+			print("")
+			count += 1
+			break
+		else:
+			pass
 
-# for data in linkname:
-# 	data = data.lower()
-# 	if (data.find('apple') != -1):
-# 		print("TRUE")
-# 	else:
-# 		print("FALSE")
+print(count)
